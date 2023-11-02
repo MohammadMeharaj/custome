9bfeddaa38807ec4a90e0349c9877bf8826074e5
MohammadMeharaj <147807340+MohammadMeharaj@users.noreply.github.com>
2023-11-02 16:03:29 +0530

    Update customeexception.java

--- customeexception.java
+++ customeexception.java
@@ -1,31 +1,49 @@
-
-class FibonacciException extends Exception {
-    public FibonacciException(String msg) {
-        super(msg);
+import java.util.InputMismatchException;
+import java.util.Scanner;
+class Ownexception extends Exception
+{
+    public Ownexception(String message)
+    {
+        super(message);
+    }
+}
+class CustomExceptionFib
+{
+    public static void main(String[]args) throws Ownexception
+    {
+        try{
+            int a=0;
+            int b=1;
+            int result=0;
+            Scanner s=new Scanner(System.in);
+            System.out.println("n:");
+            int n=s.nextInt();
+            if(n<0)
+            {
+                throw new Ownexception("No negative values");
+            }
+            if(n==0)
+            {
+                System.out.println("zero cannot be taken");
+            }
+            for(int i=1;i<=n;i++)
+            {
+                result=a+b;
+                System.out.println(" "+result);
+                a=b;
+                b=result;
+            }
+        }
+        catch(InputMismatchException e){
+            System.out.println("Enter int type only");
+        }
+        catch(Ownexception ae)
+        {
+            System.out.println("   enter a valid value"+ae.getMessage());
+        }        
     }
 }
 
-public class customeexception  {
-    public static int fibonacci(int n) throws FibonacciException {
-        if (n < 0) {
-            throw new FibonacciException("Input cannot be negative");
-        } else if (n <= 1) {
-            return n;
-        } else {
-            int a = 0;
-            int b = 1;
-            int result = 0;
-            for (int i = 2; i <= n; i++) {
-                result = a + b;
-                a = b;
-                b = result;
-            }
-            return result;
-        }
-    }
-
-    public static void main(String[] args) {
-        try {
             
             int n=-9;
             int result = fibonacci(n);
